6961250f02972db3f9d6069880c677866da5ff61
WhiteJohny <vvvfrog2004@gmail.com>
2026-02-17 15:17:28 +0300

    k8s 0, 1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DJANGO_DEBUG')
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
